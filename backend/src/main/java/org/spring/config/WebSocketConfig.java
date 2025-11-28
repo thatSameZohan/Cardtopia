c586908c2f324712247c452e0d2a5c5cba263657
thatSameZohan <king31121996@gmail.com>
2025-11-28 21:21:06 +0300

    путь WebSocket Stomp "/ws"

--- backend/src/main/java/org/spring/config/WebSocketConfig.java
+++ backend/src/main/java/org/spring/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(gameHandler, "/ws")
+        registry.addHandler(gameHandler, "/ws2")
                 .addInterceptors(new JwtHandshakeInterceptor(jwtUtil))
                 .setAllowedOrigins("*");
     }
