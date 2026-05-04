e19bff68607cdc725f3bfed318f43b0c8e0602ae
Evgen Tsygankov <tsy@mail.com>
2026-05-04 13:18:36 +0300

    test

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -8,12 +8,22 @@ const __dirname = path.dirname(__filename);
 const config = {
   poweredByHeader: false,
   reactStrictMode: true,
+  output: 'standalone',
 
   sassOptions: {
     includePaths: [path.join(__dirname, 'src', 'app', 'styles')],
   },
 
   turbopack: {},
+
+  async rewrites() {
+    const apiUrl =
+      process.env.NEXT_PUBLIC_API_URL_SERVER || 'http://backend:8080';
+    return [
+      { source: '/api/:path*', destination: `${apiUrl}/api/:path*` },
+      { source: '/ws', destination: `${apiUrl}/ws` },
+    ];
+  },
 };
 
 export default config;
